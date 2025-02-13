7a8c19abab661fb412d2cdf6cba630d57f685331
narsss1234 <narsss1234@gmail.com>
2025-02-13 22:18:45 +0530

    Added a few few more custom errors, next will be validating the input jobs data....

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -8,3 +8,27 @@ export class NotFoundError extends Error{
         this.StatusCodes = StatusCodes.NOT_FOUND;
     }
 }
+
+export class BadRequestError extends Error{
+    constructor(message){
+        super(message);
+        this.name = 'BadRequestError';
+        this.StatusCodes = StatusCodes.BAD_REQUEST;
+    }
+}
+
+export class UnauthenticatedError extends Error{
+    constructor(message) {
+        super(message);
+        this.name = 'UnauthenticatedError';
+        this.StatusCodes = StatusCodes.UNAUTHORIZED;
+    }
+}
+
+export class UnauthenticatedError extends Error{
+    constructor(message) {
+        super(message);
+        this.name = 'UnauthenticatedError';
+        this.StatusCodes = StatusCodes.UNAUTHORIZED;
+    }
+}   
